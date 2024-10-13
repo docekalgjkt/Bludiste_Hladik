052aff5e07a65944575029c4e15ab29b54320241
AdamH <adam168sh@gmail.com>
2024-10-13 23:02:21 +0200

    created robot, and altered player_draw -> character_drow for better implementation

--- bludiste.py
+++ bludiste.py
@@ -1,5 +1,7 @@
+import time
 import numpy as np
 import tkinter as tk
+
 
 class Maze:
     def __init__(self):
@@ -26,6 +28,7 @@ class Player:
         self.posy = np.where(maze == 8)[0][0]
 
     def direction(self, input):
+        # normalized (x,y)
         if input == "w":
             return 0, -1
         elif input == "a":
@@ -42,22 +45,74 @@ class Player:
         except IndexError:
             window.label.config(text="Cant go there")
         else:
-            if self.maze[self.posy + dir[1], self.posx + dir[0]] == 1 or self.posx+dir[0] < 0 or self.posy+dir[1] < 0:
+            if self.maze[self.posy + dir[1], self.posx + dir[0]] == 1 or self.posx + dir[0] < 0 or self.posy + dir[1] < 0:
                 window.label.config(text="Cant go there")
             else:
                 window.label.config(text="...")
 
                 if self.maze[self.posy + dir[1], self.posx + dir[0]] == 3:
+                    can_win = True
                     window.label.config(text="You won!")
-                    global status
-                    status = False
 
                 self.maze[self.posy + dir[1], self.posx + dir[0]] = 8
                 self.maze[self.posy, self.posx] = 0
                 self.posx +=dir[0]
                 self.posy +=dir[1]
 
-                window.player_draw()
+                window.character_draw()
+
+
+class Robot:
+    def __init__(self,maze):
+        self.maze = maze
+        self.posx = np.where(maze == 8)[1][0]
+        self.posy = np.where(maze == 8)[0][0]
+        # rightward, forward, leftward, backward
+            # normalized (x,y)
+        self.directions = [(1,0),(0,-1),(-1,0),(0,1)]
+        self.status = True
+
+    def rotate(self):
+        # shifts the tuples in directions one to the right (rotates the list as a loop)
+        self.directions = self.directions[1:] + [self.directions[0]]
+
+    def move(self):
+        while self.status:
+            to_go = []
+            for dir in self.directions:
+                try:
+                    self.maze[self.posy + dir[1], self.posx + dir[0]]
+                except IndexError:
+                    continue
+                else:
+                    if (self.posx + dir[0] >= 0 and self.posy + dir[1] >= 0) and not self.maze[self.posy + dir[1], self.posx + dir[0]] == 1:
+                        to_go.append(dir)
+
+            for dir in to_go:
+                window.label.config(text="...")
+
+                if self.maze[self.posy + dir[1], self.posx + dir[0]] == 3:
+                    window.label.config(text="You won!")
+                    self.status = False
+
+                self.maze[self.posy + dir[1], self.posx + dir[0]] = 8
+                self.maze[self.posy, self.posx] = 0
+                self.posx += dir[0]
+                self.posy += dir[1]
+
+                window.character_draw()
+                window.root.update()
+
+                altered_directions = self.directions[2:] + self.directions[:2]
+                if dir == altered_directions[0] or dir == altered_directions[1] or dir == altered_directions[2]:
+                    for i in range(altered_directions.index(dir)+1):
+                        self.rotate()
+
+
+                time.sleep(0.2)
+                break
+
+
 
 
 class Window:
@@ -76,23 +131,27 @@ class Window:
         self.frame = tk.Frame(root)
         self.frame.pack(side="right")
 
-        self.button = tk.Button(self.frame, text="W", width=10, height=4, command=lambda: player.move("w"))
-        self.button.grid(row=1,column=1)
+        if not robot_enabled:
+            self.button = tk.Button(self.frame, text="W", width=10, height=4, command=lambda: player.move("w"))
+            self.button.grid(row=1,column=1)
 
-        self.button = tk.Button(self.frame, text="A", width=10, height=4, command=lambda: player.move("a"))
-        self.button.grid(row=2,column=0)
+            self.button = tk.Button(self.frame, text="A", width=10, height=4, command=lambda: player.move("a"))
+            self.button.grid(row=2,column=0)
 
-        self.button = tk.Button(self.frame, text="S", width=10, height=4, command=lambda: player.move("s"))
-        self.button.grid(row=2,column=1)
+            self.button = tk.Button(self.frame, text="S", width=10, height=4, command=lambda: player.move("s"))
+            self.button.grid(row=2,column=1)
 
-        self.button = tk.Button(self.frame, text="D", width=10, height=4, command=lambda: player.move("d"))
-        self.button.grid(row=2,column=3)
+            self.button = tk.Button(self.frame, text="D", width=10, height=4, command=lambda: player.move("d"))
+            self.button.grid(row=2,column=3)
+        else:
+            self.button = tk.Button(self.frame, text="Start", width=10, height=4, command=robot.move)
+            self.button.pack()
 
-        self.player_rec = None
-        self.player_lab = None
+        self.character_rectangle = None
+        self.character_label = None
 
         self.maze_creation()
-        self.player_draw()
+        self.character_draw()
 
     def maze_creation(self):
         for row in range(len(maze.maze)):
@@ -110,25 +169,25 @@ class Window:
         self.canvas.create_window(x1+40, y2-40, window=tk.Label(self.canvas, text="Finish", bg="green", fg="white",
                                                                   font=("Helvetica",10)))
 
-    def player_draw(self):
-        if self.player_rec is not None:
-            self.canvas.delete(self.player_rec)
-            self.canvas.delete(self.player_lab)
-        posx = player.posy
-        posy = player.posx
+    def character_draw(self):
+        if self.character_rectangle is not None:
+            self.canvas.delete(self.character_rectangle)
+            self.canvas.delete(self.character_label)
+
+        character = robot if robot_enabled else False
+        posx = character.posy
+        posy = character.posx
         x1, y1 = posy * 100+10, posx * 100+10
         x2, y2 = x1 + 80, y1 + 80
-        self.player_rec = self.canvas.create_oval(x1, y1, x2, y2, fill="red", outline="darkred")
-        self.player_lab = self.canvas.create_window(x1+40, y2-40, window=tk.Label(self.canvas, text="Player", bg="red", fg="white",
+        self.character_rectangle = self.canvas.create_oval(x1, y1, x2, y2, fill="red", outline="darkred")
+        self.character_label = self.canvas.create_window(x1+40, y2-40, window=tk.Label(self.canvas, text="Character", bg="red", fg="white",
                                                   font=("Helvetica", 10)))
 
 
 maze = Maze()
 player = Player(maze.maze)
-
-status = False
-while status:
-    player.move(str(input("> ")))
+robot = Robot(maze.maze)
+robot_enabled = True
 
 if __name__ == "__main__":
     root = tk.Tk()
