f19678c40becfbf57c6793279f8463c694fb72e0
AdamH <adam168sh@gmail.com>
2024-10-14 22:40:53 +0200

    implemented flood fill, deleted player, elements (-labels) are resized to canvas - needs more work

--- bludiste.py
+++ bludiste.py
@@ -1,16 +1,22 @@
 import time
 import numpy as np
 import tkinter as tk
+from flood_fill_algorithm import FloodFill
 
 
 class Maze:
     def __init__(self):
         self.maze = np.array([
-            [0, 1, 1, 3, 0],
-            [0, 0, 0, 1, 0],
-            [0, 1, 8, 1, 0],
-            [0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 1]
+            [0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+            [8, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0, 1, 1, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+            [0, 0, 0, 1, 3, 0, 0, 0, 0, 0],
+            [1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ])
 
         filename = 'maze_file.npz'
@@ -19,48 +25,6 @@ class Maze:
 
         loaded_data = np.load(filename)
         self.maze = loaded_data['maze']
-        print(self.maze[1,1])
-
-
-class Player:
-    def __init__(self,maze):
-        self.maze = maze
-        self.posx = np.where(maze == 8)[1][0]
-        self.posy = np.where(maze == 8)[0][0]
-
-    def direction(self, input):
-        # normalized (x,y)
-        if input == "w":
-            return 0, -1
-        elif input == "a":
-            return -1, 0
-        elif input == "s":
-            return 0, 1
-        elif input == "d":
-            return 1, 0
-
-    def move(self, input):
-        dir = self.direction(input)
-        try:
-            self.maze[self.posy + dir[1], self.posx + dir[0]]
-        except IndexError:
-            window.label.config(text="Cant go there")
-        else:
-            if self.maze[self.posy + dir[1], self.posx + dir[0]] == 1 or self.posx + dir[0] < 0 or self.posy + dir[1] < 0:
-                window.label.config(text="Cant go there")
-            else:
-                window.label.config(text="...")
-
-                if self.maze[self.posy + dir[1], self.posx + dir[0]] == 3:
-                    can_win = True
-                    window.label.config(text="You won!")
-
-                self.maze[self.posy + dir[1], self.posx + dir[0]] = 8
-                self.maze[self.posy, self.posx] = 0
-                self.posx +=dir[0]
-                self.posy +=dir[1]
-
-                window.character_draw()
 
 
 class Robot:
@@ -68,51 +32,56 @@ class Robot:
         self.maze = maze
         self.posx = np.where(maze == 8)[1][0]
         self.posy = np.where(maze == 8)[0][0]
+        self.start_position = (np.where(maze == 8)[0][0],np.where(maze == 8)[1][0])
         # rightward, forward, leftward, backward
             # normalized (x,y)
         self.directions = [(1,0),(0,-1),(-1,0),(0,1)]
         self.status = True
 
-    def rotate(self):
-        # shifts the tuples in directions one to the right (rotates the list as a loop)
-        self.directions = self.directions[1:] + [self.directions[0]]
-
     def move(self):
-        while self.status:
-            to_go = []
-            for dir in self.directions:
-                try:
-                    self.maze[self.posy + dir[1], self.posx + dir[0]]
-                except IndexError:
-                    continue
-                else:
-                    if (self.posx + dir[0] >= 0 and self.posy + dir[1] >= 0) and not self.maze[self.posy + dir[1], self.posx + dir[0]] == 1:
-                        to_go.append(dir)
+        window.label.config(text="...")
+        alg = FloodFill(self.maze)
+        alg.flood()
+        transformed = alg.transformed
+        current_position = self.start_position
+        start_time = time.time()
 
-            for dir in to_go:
-                window.label.config(text="...")
-
-                if self.maze[self.posy + dir[1], self.posx + dir[0]] == 3:
-                    window.label.config(text="You won!")
-                    self.status = False
-
-                self.maze[self.posy + dir[1], self.posx + dir[0]] = 8
-                self.maze[self.posy, self.posx] = 0
-                self.posx += dir[0]
-                self.posy += dir[1]
-
-                window.character_draw()
-                window.root.update()
-
-                altered_directions = self.directions[2:] + self.directions[:2]
-                if dir == altered_directions[0] or dir == altered_directions[1] or dir == altered_directions[2]:
-                    for i in range(altered_directions.index(dir)+1):
-                        self.rotate()
-
-
-                time.sleep(0.2)
+        while True:
+            # if reached the goal
+            if transformed[current_position[0], current_position[1]] == 1:
+                print("Reached the destination!")
+                window.label.config(text=f"{time.time()-start_time:.1f}s")
                 break
 
+            next_position = None
+            min_value = np.inf
+
+            # checks neighboring cells for the next move
+            for dir in self.directions:
+                new_y = current_position[0] + dir[0]
+                new_x = current_position[1] + dir[1]
+
+                # checks bounds
+                if 0 <= new_y < transformed.shape[0] and 0 <= new_x < transformed.shape[1]:
+                    cell_value = transformed[new_y, new_x]
+
+                    # searching for the lowest non-infinity value
+                    if min_value > cell_value > 0:  # avoids walls (inf.) and only considers cells filled with legal number
+                        min_value = cell_value
+                        next_position = (new_y, new_x)
+
+            if next_position is None:  # If no valid moves are available, we're stuck
+                window.label.config(text=f"Can't solve this maze")
+                break
+
+            current_position = next_position  # Move to the next position
+            self.posx = next_position[1]
+            self.posy = next_position[0]
+
+            window.character_draw()
+            window.root.update()
+
+            time.sleep(0.2)
 
 
 
@@ -121,9 +90,9 @@ class Window:
         self.root = root
         self.root.title("Maze")
 
-        width = len(maze.maze[0])*100-10
-        height = len(maze.maze)*100-10
-        self.canvas = tk.Canvas(root, width=width, height=height, bg="white", relief="solid", border=5)
+        self.width = (100/len(maze.maze[0]))*50
+        self.height = (100/len(maze.maze))*50
+        self.canvas = tk.Canvas(root, width=self.width, height=self.height, bg="white", relief="solid", border=5)
         self.canvas.pack(side="left")
 
         self.label = tk.Label(root, text="...", font=("Helvetica", 16))
@@ -132,21 +101,8 @@ class Window:
         self.frame = tk.Frame(root)
         self.frame.pack(side="right")
 
-        if not robot_enabled:
-            self.button = tk.Button(self.frame, text="W", width=10, height=4, command=lambda: player.move("w"))
-            self.button.grid(row=1,column=1)
-
-            self.button = tk.Button(self.frame, text="A", width=10, height=4, command=lambda: player.move("a"))
-            self.button.grid(row=2,column=0)
-
-            self.button = tk.Button(self.frame, text="S", width=10, height=4, command=lambda: player.move("s"))
-            self.button.grid(row=2,column=1)
-
-            self.button = tk.Button(self.frame, text="D", width=10, height=4, command=lambda: player.move("d"))
-            self.button.grid(row=2,column=3)
-        else:
-            self.button = tk.Button(self.frame, text="Start", width=10, height=4, command=robot.move)
-            self.button.pack()
+        self.button = tk.Button(self.frame, text="Start", width=10, height=4, command=robot.move)
+        self.button.pack()
 
         self.character_rectangle = None
         self.character_label = None
@@ -155,40 +111,46 @@ class Window:
         self.character_draw()
 
     def maze_creation(self):
+        print(self.width/len(maze.maze[0]))
+        print(self.height/len(maze.maze))
         for row in range(len(maze.maze)):
             for col in range(len(maze.maze[row])):
                 color = "black" if maze.maze[row][col] == 1 else "white"
-                x1, y1 = col * 100, row * 100
-                x2, y2 = x1 + 100, y1 + 100
+                x1, y1 = col * self.width/len(maze.maze[0])+5, row * self.height/len(maze.maze)+5
+                x2, y2 = x1 + self.width/len(maze.maze[0])+5, y1 + self.height/len(maze.maze)+5
                 self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="")
 
         posy = np.where(maze.maze == 3)[1][0]
         posx = np.where(maze.maze == 3)[0][0]
-        x1, y1 = posy * 100 + 10, posx * 100 + 10
-        x2, y2 = x1 + 80, y1 + 80
+        x1, y1 = (posy * self.width/len(maze.maze[0])+5,
+                  posx * self.height/len(maze.maze)+5)
+        x2, y2 = (x1 + self.width/len(maze.maze[0]),
+                  y1 + self.height/len(maze.maze))
         self.canvas.create_rectangle(x1, y1, x2, y2, fill="green", outline="lightgreen")
-        self.canvas.create_window(x1+40, y2-40, window=tk.Label(self.canvas, text="Finish", bg="green", fg="white",
-                                                                  font=("Helvetica",10)))
+        self.canvas.create_window(x1+self.width/len(maze.maze[0])/2.3, y2-self.width/len(maze.maze)/2.3,
+                                  window=tk.Label(self.canvas, text="Finish", bg="green", fg="white",
+                                                                  font=("Helvetica",int(self.width/len(maze.maze)/10))))
 
     def character_draw(self):
         if self.character_rectangle is not None:
             self.canvas.delete(self.character_rectangle)
             self.canvas.delete(self.character_label)
 
-        character = robot if robot_enabled else False
+        character = robot
         posx = character.posy
         posy = character.posx
-        x1, y1 = posy * 100+10, posx * 100+10
-        x2, y2 = x1 + 80, y1 + 80
+        x1, y1 = (posy * self.width / len(maze.maze[0]) + 5,
+                  posx * self.height / len(maze.maze) + 5)
+        x2, y2 = (x1 + self.width / len(maze.maze[0]),
+                  y1 + self.height / len(maze.maze))
         self.character_rectangle = self.canvas.create_oval(x1, y1, x2, y2, fill="red", outline="darkred")
-        self.character_label = self.canvas.create_window(x1+40, y2-40, window=tk.Label(self.canvas, text="Character", bg="red", fg="white",
-                                                  font=("Helvetica", 10)))
+        self.character_label = self.canvas.create_window(x1+self.width/len(maze.maze[0])/2.3, y2-self.width/len(maze.maze)/2.3,
+                                                         window=tk.Label(self.canvas, text="Character", bg="red", fg="white",
+                                                  font=("Helvetica", int(self.width/len(maze.maze)/10))))
 
 
 maze = Maze()
-player = Player(maze.maze)
 robot = Robot(maze.maze)
-robot_enabled = True
 
 if __name__ == "__main__":
     root = tk.Tk()
--- flood_fill_algorithm.py
+++ flood_fill_algorithm.py
@@ -1,26 +1,6 @@
 import numpy as np
 from collections import deque
 
-class Maze:
-    def __init__(self):
-        self.maze = np.array([
-            [0, 1, 1, 3, 0],
-            [0, 0, 0, 1, 0],
-            [0, 1, 8, 1, 0],
-            [0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 1]
-        ])
-
-        filename = 'maze_file.npz'
-
-        np.savez(filename, maze=self.maze)
-
-        loaded_data = np.load(filename)
-        self.maze = loaded_data['maze']
-
-maze = Maze()
-
-
 class FloodFill:
     def __init__(self,maze):
         self.maze = maze
@@ -28,6 +8,7 @@ class FloodFill:
         self.finish = np.where(maze == 3)[0][0], np.where(maze == 3)[1][0]
         self.directions = [(1, 0), (0, -1), (-1, 0), (0, 1)]
         self.transformed = self.maze_transform()
+        self.can_be_solved = True
 
     def maze_transform(self):
         transformed = self.maze.astype(float)
@@ -37,26 +18,21 @@ class FloodFill:
         return transformed
 
     def flood(self, position=None):
-        queue = deque([self.finish])  # Start BFS with the finish point
-        self.transformed[self.finish[0], self.finish[1]] = 1  # Start with 1 at the finish point
+        queue = deque([self.finish])  # creates queue and adds finish to it
+        self.transformed[self.finish[0], self.finish[1]] = 1  # replaces finish with 1
 
         while queue:
-            current = queue.popleft()  # Get the current position
+            current = queue.popleft()  # gets the first to be explored position and removes it
             current_value = self.transformed[current[0], current[1]]
 
-            # Explore neighbors
+            # explores neighbors
             for dir in self.directions:
-                new_x = current[0] + dir[0]
-                new_y = current[1] + dir[1]
+                new_y = current[0] + dir[0]
+                new_x = current[1] + dir[1]
 
-                # Check if the new position is within bounds
-                if 0 <= new_x < self.transformed.shape[0] and 0 <= new_y < self.transformed.shape[1]:
-                    # If the neighboring cell is unvisited (i.e., 0 or infinity), fill it with current_value + 1
-                    if self.transformed[new_x, new_y] == 0:
-                        self.transformed[new_x, new_y] = current_value + 1
-                        queue.append((new_x, new_y))  # Add the new position to the queue for further exploration
-
-
-algo = FloodFill(maze.maze)
-algo.flood()
-print(algo.transformed)
+                # checks bounds
+                if 0 <= new_y < self.transformed.shape[0] and 0 <= new_x < self.transformed.shape[1]:
+                    # if the neighboring cell is unvisited, fill it with current_value + 1
+                    if self.transformed[new_y, new_x] == 0:
+                        self.transformed[new_y, new_x] = current_value + 1
+                        queue.append((new_y, new_x))  # add the new position to the queue for further exploration in next iteration
