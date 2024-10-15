6d9d4dde8390eb5957f281b4059530802611fad2
AdamH <adam168sh@gmail.com>
2024-10-15 14:33:22 +0200

    crazy class creation

--- bludiste.py
+++ bludiste.py
@@ -116,8 +116,8 @@ class MazeView:
         self.root = root
         self.root.title("Maze")
 
-        self.width = (100/len(maze.maze[0]))*50
-        self.height = (100/len(maze.maze))*50
+        self.width = (100/maze.size[0]*50)
+        self.height = (100/maze.size[1]*50)
         self.canvas = tk.Canvas(root, width=self.width, height=self.height, bg="white", relief="solid", border=5)
         self.canvas.pack(side="left")
 
@@ -130,16 +130,16 @@ class MazeView:
         for row in range(len(maze.maze)):
             for col in range(len(maze.maze[row])):
                 color = "black" if maze.maze[row][col] == 1 else "white"
-                x1, y1 = col * self.width/len(maze.maze[0])+5, row * self.height/len(maze.maze)+5
-                x2, y2 = x1 + self.width/len(maze.maze[0])+5, y1 + self.height/len(maze.maze)+5
+                x1, y1 = col * self.width/maze.size[1]+5, row * self.height/maze.size[0]+5
+                x2, y2 = x1 + self.width/maze.size[1]+5, y1 + self.height/maze.size[0]+5
                 self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="")
 
-        posy = np.where(maze.maze == 3)[1][0]
-        posx = np.where(maze.maze == 3)[0][0]
-        x1, y1 = (posy * self.width/len(maze.maze[0])+(self.width/len(maze.maze[0])/5),
-                  posx * self.height/len(maze.maze)+(self.height/len(maze.maze[0])/5))
-        x2, y2 = (x1 + (self.width/len(maze.maze[0]))-(self.width/len(maze.maze[0])/5),
-                  y1 + (self.height/len(maze.maze[0]))-(self.height/len(maze.maze[0])/5))
+        posy = maze.finish[1]
+        posx = maze.finish[0]
+        x1, y1 = (posy * self.width/maze.size[1]+(self.width/maze.size[1]/5),
+                  posx * self.height/maze.size[0]+(self.height/maze.size[0]/5))
+        x2, y2 = (x1 + (self.width/maze.size[1])-(self.width/maze.size[1]/5),
+                  y1 + (self.height/maze.size[1])-(self.height/maze.size[1]/5))
         self.canvas.create_rectangle(x1, y1, x2, y2, fill="green", outline="lightgreen")
 
 
@@ -151,10 +151,10 @@ class MazeView:
         character = robot
         posx = character.posy
         posy = character.posx
-        x1, y1 = (posy * self.width / len(maze.maze[0]) + (self.width / len(maze.maze[0]) / 5),
-                  posx * self.height / len(maze.maze) + (self.height / len(maze.maze[0]) / 5))
-        x2, y2 = (x1 + (self.width / len(maze.maze[0])) - (self.width / len(maze.maze[0]) / 5),
-                  y1 + (self.height / len(maze.maze[0])) - (self.height / len(maze.maze[0]) / 5))
+        x1, y1 = (posy * self.width / maze.size[1] + (self.width / maze.size[1] / 5),
+                  posx * self.height / maze.size[0] + (self.height / maze.size[0] / 5))
+        x2, y2 = (x1 + (self.width / maze.size[1]) - (self.width / maze.size[1] / 5),
+                  y1 + (self.height / maze.size[1]) - (self.height / maze.size[1] / 5))
         self.character_rectangle = self.canvas.create_oval(x1, y1, x2, y2, fill="red", outline="darkred")
 
 
